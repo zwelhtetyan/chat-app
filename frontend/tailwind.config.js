4e2f3f6e24246e17d3f6e5fb0703369c52df54bb
Nue <phonehtetaung1993@gmail.com>
2023-02-22 01:49:28 +0630

    write for main chat page at index.html

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      color: {
+        "line-input": "#E6E6E6",
+      },
+      boxShadow: {
+        "swipe-shadow": "-129px -5px 56px 144px rgba(0,0,0,0.64)",
+      },
+      screens: {
+        "ph-L": "425px",
+      },
+    },
   },
   plugins: [],
 };
