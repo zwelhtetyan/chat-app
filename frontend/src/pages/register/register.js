3801cbc2c8a9f7cb2600db074e02c3b2f7bf26bf
wminaung <minwin243@gmail.com>
2023-02-22 23:56:50 +0630

    implemention auth session in frontend

--- frontend/src/pages/register/register.js
+++ frontend/src/pages/register/register.js
@@ -21,3 +21,39 @@ updateLabelOnBlur(emailInput, emailInputLabel);
 
 updateLabelOnFocus(passwordInput, passwordInputLabel);
 updateLabelOnBlur(passwordInput, passwordInputLabel);
+
+//////////// registration process /////////
+
+const registerBtn = document.getElementById("registerBtn");
+registerBtn.addEventListener("click", async () => {
+  const { name, email, password } = {
+    name: nameInput.value,
+    email: emailInput.value,
+    password: passwordInput.value,
+  };
+
+  console.log(name, email, password);
+  if (!name || !email || !password) {
+    return;
+  }
+
+  const res = await fetch(`${window.location.origin}/registerUser`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ name, email, password }),
+  });
+
+  const { status, message, redirectUrl } = await res.json();
+
+  if (status === "success") {
+    alert(message); //
+    if (redirectUrl) {
+      window.location.href = redirectUrl; // --> /login
+    }
+  } else {
+    alert(message); //
+    console.log(message);
+  }
+});
