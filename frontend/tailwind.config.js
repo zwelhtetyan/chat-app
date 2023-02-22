f35b2b84ab208e1483d03c2e6a6341811a206f7d
Nue <phonehtetaung1993@gmail.com>
2023-02-22 22:34:27 +0630

    javascript code for responsive design

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -13,6 +13,28 @@ module.exports = {
         xs: "480px",
         "ph-L": "425px",
       },
+      keyframes: {
+        fC: {
+          "0%": { backgroundColor: "#D9D9D9" },
+          "20%": { backgroundColor: "#A6A6A6" },
+          "40%": { backgroundColor: "#D9D9D9" },
+        },
+        sC: {
+          "20%": { backgroundColor: "#D9D9D9" },
+          "40%": { backgroundColor: "#A6A6A6" },
+          "60%": { backgroundColor: "#D9D9D9" },
+        },
+        tC: {
+          "40%": { backgroundColor: "#D9D9D9" },
+          "60%": { backgroundColor: "#A6A6A6" },
+          "90%": { backgroundColor: "#D9D9D9" },
+        },
+      },
+      animation: {
+        fC: "fC 1.5s ease-in-out infinite",
+        sC: "fC 1.5s ease-in-out infinite",
+        tC: "fC 1.5s ease-in-out infinite",
+      },
     },
   },
   plugins: [],
