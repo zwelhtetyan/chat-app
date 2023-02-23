a9dc0a6cbfe8a9a06d6863e8fdf55d6534dffb37
Nue <phonehtetaung1993@gmail.com>
2023-02-24 00:49:40 +0630

    send message with enter button and to show active or offline

--- backend/index.ts
+++ backend/index.ts
@@ -1,11 +1,12 @@
 import express, { Request, Response } from "express";
 import { join } from "path";
-import { Server } from "socket.io";
+import { Server, Socket } from "socket.io";
 import { readFileSync, writeFileSync } from "fs";
 import { loginRouter } from "./routes/loginRouter";
 import { registerRouter } from "./routes/registerRouter";
 import { chatHistory } from "./models/history";
-import { users } from "./models/users";
+import usersData from "./database/users.json";
+// import { users } from "./models/users";
 import { homeRouter } from "./routes/homeRouter";
 
 const PORT = process.env.PORT || 8080;
@@ -34,6 +35,62 @@ const server = app.listen(PORT, () => {
 /// it just tesing, you can delete it
 
 let status: { socketId: string; name: string }[] = [];
+
+const io = new Server(server);
+
+io.on("connection", (socket: Socket) => {
+  // listen for offline users
+  socket.on("disconnect", () => {
+    const offlineUser = usersData.filter((user) => {
+      return user.socketId === socket.id;
+    });
+    console.log(offlineUser);
+    if (offlineUser[0]) {
+      offlineUser[0].active = false;
+    }
+    usersStatusArray(io);
+  });
+
+  // listen for active users
+  socket.on("active", (name) => {
+    const activeUser = usersData.filter((user) => {
+      return user.name === name;
+    });
+    activeUser[0].active = true;
+    activeUser[0].socketId = socket.id;
+    console.log("user's socket id", activeUser[0].socketId);
+    usersStatusArray(io);
+  });
+
+  // listen for message
+  socket.on("chat", (data) => {
+    io.sockets.emit("resData", data);
+  });
+
+  // listen for typing
+  socket.on("typing", (name) => {
+    socket.broadcast.emit("typingPs", name);
+  });
+
+  interface UsersStatus {
+    name: string;
+    active: boolean;
+  }
+
+  // functions
+
+  // functions for users's status update
+  function usersStatusArray(socketIo: Server) {
+    const usersStatus: UsersStatus[] = usersData.map((user) => {
+      return {
+        name: user.name,
+        active: user.active,
+      };
+    });
+    socketIo.sockets.emit("usersStatus", usersStatus);
+    console.log(usersStatus);
+  }
+});
 
 // const io = new Server(server);
 
