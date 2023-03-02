35cc5397f74b907d07aa79d95841b7b78b4e4cc2
Nue <phonehtetaung1993@gmail.com>
2023-03-02 22:09:50 +0630

    upgrade the user register and login

--- backend/controllers/registerController.ts
+++ backend/controllers/registerController.ts
@@ -3,10 +3,19 @@ import { join } from "path";
 import { usersModel } from "../models/users";
 import { v4 as uuidv4 } from "uuid";
 import { writeFileSync } from "fs";
+import { validateSignup } from "../models/validator";
+const sgMail = require("@sendgrid/mail");
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const sendGridApiKey = process.env.SENDGRID_API_KEY;
+sgMail.setApiKey(sendGridApiKey);
 
 interface ResponseRegisterUser {
   status: "success" | "fail";
   message: string;
+  userEmail?: string;
   redirectUrl?: string;
 }
 
@@ -18,48 +27,142 @@ interface RequestRegisterUser {
   socketId: string;
 }
 
-const registerUser = (req: Request, res: Response) => {
+let verifyId: string;
+
+const users = usersModel.getUsersData();
+
+let newUser: {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+  active: false;
+  socketId: "";
+  userImg: "";
+};
+
+function resForErr(msg: string, res: Response) {
+  const response: ResponseRegisterUser = {
+    status: "fail",
+    message: msg,
+  };
+  return res.status(200).send(response);
+}
+
+const registerUser = async (req: Request, res: Response) => {
   const { name, email, password }: RequestRegisterUser = req.body;
 
-  if (password.length < 3) {
-    const response: ResponseRegisterUser = {
-      status: "fail",
-      message: "password must have at least 3 or more characters",
-    };
-    return res.status(200).send(response);
-  }
+  const { error } = validateSignup({ email, password });
 
-  const users = usersModel.getUsersData();
+  if (error) {
+    resForErr(error.message, res);
+  }
 
   const hasUser = users.some(
     (user) => user.name === name || user.email === email
   ); // check user exist
 
   if (hasUser) {
-    const response: ResponseRegisterUser = {
-      status: "fail",
-      message: "username or email  already exist!",
-    };
-    return res.send(response);
+    const errMsg = "username or email  already exist!";
+    resForErr(errMsg, res);
   } else {
-    users.push({
+    ////////////// send code to user to verify ///////////////////
+
+    verifyId = gnrVerifyId(6);
+    console.log("verify Id", verifyId);
+
+    const response: ResponseRegisterUser = {
+      status: "success",
+      message: "email and username can be available",
+      userEmail: email,
+      redirectUrl: "/verify",
+    };
+
+    newUser = {
       id: uuidv4(),
-      name,
-      email,
-      password,
+      name: name,
+      email: email,
+      password: password,
       active: false,
       socketId: "",
       userImg: "",
-    });
+    };
+
+    const message = {
+      to: email,
+      from: "nuebusiness382@gmail.com",
+      subject: "verify code",
+      text: `This is your OTP verify code: ${verifyId}`,
+      html: `<strong>This is your OTP verify code: ${verifyId}</strong>`,
+    };
+
+    await sgMail.send(message).then(
+      () => {
+        console.log("sended email");
+      },
+      (error: any) => {
+        console.error(error);
+
+        if (error.response) {
+          console.error(error.response.body);
+        }
+      }
+    );
+    ////////////////////// after verify //////////////////////////
+    // users.push({
+    //   id: uuidv4(),
+    //   name,
+    //   email,
+    //   password,
+    //   active: false,
+    //   socketId: "",
+    //   userImg: "",
+    // });
+    // usersModel.setUsersData(users); // set Users Data
+    // const response: ResponseRegisterUser = {
+    //   status: "success",
+    //   message: "registration successfully!",
+    //   redirectUrl: "/login",
+    // };
+    return res.status(200).send(response);
+  }
+};
+
+const verifiedEmail = (req: Request, res: Response) => {
+  const reqOtpCode = req.body;
+  console.log(reqOtpCode.otpCode === verifyId);
+  if (reqOtpCode.otpCode === verifyId) {
+    users.push(newUser);
     usersModel.setUsersData(users); // set Users Data
     const response: ResponseRegisterUser = {
       status: "success",
       message: "registration successfully!",
       redirectUrl: "/login",
     };
-    return res.status(200).send(response);
+    res.status(200).send(response);
+  } else {
+    const errMsg = "fill the right OTP code";
+    resForErr(errMsg, res);
   }
+
+  console.log("using verify function");
+  // res.send({ data: "otp code arrive to backend" });
 };
+
+function gnrVerifyId(length: number) {
+  let result = "";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const charactersLength = characters.length;
+  let counter = 0;
+  while (counter < length) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    counter += 1;
+  }
+  return result.toUpperCase();
+}
+
+console.log(gnrVerifyId(5));
 
 const showRegister = (req: Request, res: Response) => {
   return res
@@ -78,4 +181,4 @@ const showRegister = (req: Request, res: Response) => {
     );
 };
 
-export { registerUser, showRegister };
+export { registerUser, showRegister, verifiedEmail };
--- backend/routes/registerRouter.ts
+++ backend/routes/registerRouter.ts
@@ -1,9 +1,14 @@
 import express from "express";
-import { registerUser, showRegister } from "../controllers/registerController";
+import {
+  registerUser,
+  showRegister,
+  verifiedEmail,
+} from "../controllers/registerController";
 
 const registerRouter = express.Router();
 
 registerRouter.get("/register", showRegister);
 registerRouter.post("/registerUser", registerUser);
+registerRouter.post("/verifiedEmail", verifiedEmail);
 
 export { registerRouter };
